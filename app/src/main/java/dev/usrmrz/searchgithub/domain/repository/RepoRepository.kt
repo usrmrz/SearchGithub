28ba96abff6c536a55ecd38489093dcf848c1115
usrmrz <usrmrz@gmail.com>
2025-01-28 19:49:15 +0200

    delete unused imports

--- app/src/main/java/dev/usrmrz/searchgithub/domain/repository/RepoRepository.kt
+++ app/src/main/java/dev/usrmrz/searchgithub/domain/repository/RepoRepository.kt
@@ -1,6 +1,6 @@
 package dev.usrmrz.searchgithub.domain.repository
 
-import dev.usrmrz.searchgithub.data.database.entity.RepoEntity
+import dev.usrmrz.searchgithub.domain.model.Repo
 import dev.usrmrz.searchgithub.domain.model.Resource
 import kotlinx.coroutines.flow.Flow
 
